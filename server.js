dede6152b93bc0c69454c42ff77716ccf49233b9
Javier Perez <javier21perez98@gmail.com>
2021-05-05 15:30:16 -0700

    useUnifiedTopology: true added to server js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
 	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 // Start the server
