ed47d29090cd77b251866f6d8b989a2cc50a9972
Javier Perez <javier21perez98@gmail.com>
2021-05-06 15:13:55 -0700

    updated homeroutes

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -3,13 +3,13 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, ".././public/html/index.html"));
+	res.sendFile(path.join(__dirname, "../public/html/index.html"));
 });
 router.get("/stats", function (req, res) {
-	res.sendFile(path.join(__dirname, ".././public/html/stats.html"));
+	res.sendFile(path.join(__dirname, "../public/html/stats.html"));
 });
 router.get("/exercise", function (req, res) {
-	res.sendFile(path.join(__dirname, ".././public/html/exercise.html"));
+	res.sendFile(path.join(__dirname, "../public/html/exercise.html"));
 });
 
 module.exports = router;
