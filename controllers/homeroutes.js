879756a1877a57170c9807d79f29f1b7ef1163cc
Javier Perez <javier21perez98@gmail.com>
2021-05-05 17:49:37 -0700

    added univeral route to redirct to homepage

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -11,5 +11,8 @@ router.get("/stats", function (req, res) {
 router.get("/exercise", function (req, res) {
 	res.sendFile(path.join(__dirname, ".././public/html/exercise.html"));
 });
+router.get("*", function (req, res) {
+	res.redirect("/");
+});
 
 module.exports = router;
