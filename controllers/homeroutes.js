1b645ed301e9d2e9aa5e1d5ecfbc394b37e48627
Javier Perez <javier21perez98@gmail.com>
2021-05-05 17:44:36 -0700

    added route to diplay stats page

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -5,5 +5,8 @@ const router = require("express").Router();
 router.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, ".././public/html/index.html"));
 });
+router.get("/stats", function (req, res) {
+	res.sendFile(path.join(__dirname, ".././public/html/stats.html"));
+});
 
 module.exports = router;
